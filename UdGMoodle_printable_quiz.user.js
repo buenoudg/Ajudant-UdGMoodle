2a442dfe19f4e19d194a36218beaa0dccd0e0783
Antonio Bueno <antonio.bueno@udg.edu>
2022-10-17 17:42:44 +0200

    Minor code formatting changes

--- UdGMoodle_printable_quiz.user.js
+++ UdGMoodle_printable_quiz.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        UdGMoodle Printable Quiz
-// @version     0.2
-// @author      Antonio Bueno
-// @namespace   antonio.bueno@udg.edu
+// @version     0.2.1
+// @author      Antonio Bueno <antonio.bueno@udg.edu>
+// @namespace   bueno.bcds.udg.edu
 // @match       https://moodle2.udg.edu/mod/quiz/attempt.php*
 // @icon        https://raw.githubusercontent.com/buenoudg/Ajudant-UdGMoodle/master/udgmoodle_44x44.png
 // @require     https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
@@ -11,8 +11,7 @@
 // @grant       GM.getResourceUrl
 // ==/UserScript==
 
-// jshint esversion:11
-
+// jshint esversion: 8
 (function () {
     "use strict";
 
@@ -23,31 +22,39 @@
     // Modificacions are applied once the DOM has been build (images may be still loading)
     $(document).ready(function () {
 
-        // Makes clear the script is running
-        notification(GM.info.script.name, "hello", 3);
+        // Makes clear that the script is running and its version
+        notification(`${GM.info.script.name} ${GM.info.script.version}`, "hello", 3);
 
         // Technical info to console
         console.log(GM.info);
 
-        $('#mod_quiz_navblock').prepend('<button id="printable">Fes-ho imprimible');
-        $('#printable').on('click', function () {
-            $('body').prepend($('#region-main'));
-            $('#responseform').prepend('<div class="que"><div class="formulation"><p contenteditable="true"><i>Text editable');
-            // Extra CSS
+        // Trigger button
+        $("#mod_quiz_navblock").prepend('<button id="printable">Fes-ho imprimible');
+        // Button behavior
+        $("#printable").on("click", function () {
+            // Move quiz container to the top
+            $("body").prepend($("#region-main"));
+            // Add editable block at the beginning of the quiz container
+            $("#responseform").prepend('<div class="que"><div class="formulation"><p contenteditable="true">' +
+                                       "<i><b>Text editable</b> (es pot copiar i enganxar des de Word i similars)");
+            // Modify webpage appearance
             let stylesheet = document.createElement("style");
             stylesheet.textContent = `
+                /* Hide elements */
                 nav, #nav-drawer, #nav-drawer-footer, .activity-navigation,
                 #page-wrapper, #top-footer, .que .info, .submitbtns, .multichoice .prompt { display: none !important }
 
+                /* Adjust spacing */
                 body, .que .content { margin: 0 }
                 #region-main>.card { border: none }
                 .card-body { padding: 0 !important }
                 .que { margin: 1em auto }
                 .que .qtext { margin-bottom: 0.5em }
 
+                /* Question numbering */
                 body { counter-reset: question }
                 .que .qtext::before { counter-increment: question; content: counter(question) ". "; font-weight: bold }
-            `; // numbered questions
+            `;
             document.documentElement.appendChild(stylesheet);
         });
 
@@ -68,6 +75,7 @@
     stylesheet.textContent = `
         div.toastify { margin: inherit; padding-bottom: 20px; width: inherit; font-family:
             -apple-system,system-ui,BlinkMacSystemFont,"Segoe UI",Roboto,"Helvetica Neue",Arial,sans-serif; }
+        /* Trigger button appearance */
         #printable::before { content: "🖨 "; font-size: 200%; position: relative; top: 0.1em }
         #printable { border-color: lightgrey darkgrey darkgrey lightgrey; padding: 0em 1em 0.5em }
     `;
@@ -84,19 +92,19 @@
         switch (type) {
             case "warning":
                 color = "rgba(201, 201, 0, 0.8)";
-                icon = '⚠️';
+                icon = "⚠️";
                 break;
             case "error":
                 color = "rgba(201, 51, 51, 0.8)";
-                icon = '🛑';
+                icon = "🛑";
                 break;
             case "hello":
                 color = "rgba(51, 153, 51, 0.8)";
-                icon = '👋🏼';
+                icon = "👋🏼";
                 break;
             default:
                 color = "rgba(51, 51, 153, 0.8)";
-                icon = 'ℹ️';
+                icon = "ℹ️";
         }
         Toastify({
             text: icon + " " + message,
