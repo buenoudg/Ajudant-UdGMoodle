a41f7600a9e33456a56f93f3c726471c59b53c5c
Antonio Bueno <antonio.bueno@udg.edu>
2022-11-22 09:56:16 +0100

    v0.7.0: Compatibilitat amb rols no editors
    A més fa servir l'idioma d'UdGMoodle enlloc del del navegador, i s'ha retocat l'aspecte dels botons

--- udgmoodle_participants_student_lists.user.js
+++ udgmoodle_participants_student_lists.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
-// @name            Participants: Llistats d'estudiants
-// @name:ca         Participants: Llistats d'estudiants
+// @name            Participants: Students' lists
+// @name:ca         Participants: Llistes d'estudiants
 // @name:en         Participants: Students' lists
-// @name:es         Participantes: Listados de estudiantes
-// @version         0.6.0
+// @name:es         Participantes: Listas de estudiantes
+// @version         0.7.0
 // @author          Antonio Bueno <antonio.bueno@udg.edu>
 // @description     Generates student/group and student/teacher lists
-// @description:ca  Genera llistats estudiant/grup i estudiant/professor
+// @description:ca  Genera llistes estudiant/grup i estudiant/professor
 // @description:en  Generates student/group and student/teacher lists
-// @description:es  Genera listados estudiante/grupo y estudiante/profesor
+// @description:es  Genera listas estudiante/grupo y estudiante/profesor
 // @license         MIT
 // @namespace       https://github.com/buenoudg/Ajudant-UdGMoodle
 // @supportURL      https://github.com/buenoudg/Ajudant-UdGMoodle/issues
@@ -24,18 +24,18 @@
 (function () {
     "use strict";
 
-    let courseId, roles, groups, fullListUrl; // defined once we have the DOM
-
-    const locales = { // localization of text in buttons and downloaded file names
+    const langs = { // localization of text in buttons, having no role, and downloaded file names
         "ca": {
             "ALL_TEXT": "Mostra'ls tots",
-            "SGL_TEXT": "Llistat estudiant/grup",
-            "SPL_TEXT": "Llistat estudiant/professor",
+            "NO_GROUP": "Sense grups",
+            "SGL_TEXT": "Llista estudiant/grup",
+            "SPL_TEXT": "Llista estudiant/professor",
             "SGL_FILE": "estudiant_grup",
             "SPL_FILE": "estudiant_professor"
         },
         "en": {
             "ALL_TEXT": "Show them all",
+            "NO_GROUP": "No groups",
             "SGL_TEXT": "Student/group list",
             "SPL_TEXT": "Student/professor list",
             "SGL_FILE": "student_group",
@@ -43,13 +43,15 @@
         },
         "es": {
             "ALL_TEXT": "Mostrarlos todos",
-            "SGL_TEXT": "Listado estudiante/grupo",
-            "SPL_TEXT": "Listado estudiante/profesor",
+            "NO_GROUP": "No hay grupos",
+            "SGL_TEXT": "Lista estudiante/grupo",
+            "SPL_TEXT": "Lista estudiante/profesor",
             "SGL_FILE": "estudiante_grupo",
             "SPL_FILE": "estudiante_profesor"
         },
     };
-    const locale = locales[navigator.language.slice(0, 2)] || locales.en;
+
+    const info = {}; // to be filled with static page details once the DOM is ready
 
     const tableToCsvDataUrl = (table) => { // used to generate the downloadable files
         const decimalDelimiter = new Intl.NumberFormat(Navigator.language).format(1.1).charAt(1);
@@ -57,25 +59,18 @@
         return `data:text/csv;charset=utf-8,\uFEFF${encodeURIComponent(csv)}`; // MS Excel needs the \uFEFF
     };
 
-    const mostCommonValue = (list) => { // used to determine the student's role name
-        const histogram = list.reduce((sum, i) => Object.assign(sum, { [i]: (sum[i] | 0) + 1 }), {});
-        return Object.keys(histogram).find(key => histogram[key] == Math.max(...Object.values(histogram)));
-    };
-
-    const getSelectOptions = (selector) => // used to determine grup and role IDs and names
-        [...document.querySelectorAll(`${selector} option`)].reduce((ob, op) => (ob[op.value] = op.text, ob), {});
-
     const getUserData = () => { // used to extract all the relevant data from the main table
         let users = [];
         $("#participants tbody tr").not(".emptyrow").each(function () {
-            const $nameColumn = $(this).find("th"); // usually the first column, but it depends on permissions
-            users.push({
-                "name": $nameColumn.text().trim(),
-                "id": $nameColumn.next().text().trim(),
-                "mail": $nameColumn.next().next().text().trim(),
-                "role": roles[$nameColumn.next().next().next().find("span").data().value],
-                "groups": $nameColumn.next().next().next().next().find("span").data().value.map(id => groups[id]),
-            });
+            const $cells = $(this).children();
+            let user = { "name": $cells.eq(info.column.fullname).text().trim() };
+            if (info.column.email >= 0) user.mail = $cells.eq(info.column.email).text().trim();
+            user.groups = $cells.eq(info.column.groups).text().replace(info.lang.NO_GROUP, "").trim();
+            user.groups = (user.groups != "") ? user.groups.split(", ") : [];
+            if (info.column.idnumber >= 0) user.id = $cells.eq(info.column.idnumber).text().trim();
+            user.role = $cells.eq(info.column.roles).text().trim();
+            if (user.role == info.noRole) user.role = undefined;
+            users.push(user);
         });
         return users;
     };
@@ -84,22 +79,21 @@
 
         // (Re-)generate the lists
         let users = getUserData(), obj;
-        const studentRole = mostCommonValue(users.map(user => user.role));
-        const professors = users.filter(user => user.role && (user.role != studentRole) && user.groups.length);
-        let classGroups = {}; // groups where some professor is a member
-        professors.forEach(function (professor) { // TODO: do this the functional way
+        const professors = users.filter(user => user.role && (user.role != info.studentRole) && user.groups.length);
+        let groups = {}; // groups where some professor is a member
+        professors.forEach(function (professor) {
             professor.groups.forEach(function (group) {
-                if (!classGroups[group]) classGroups[group] = [];
-                classGroups[group].push([professor.name, professor.id]);
+                if (!groups[group]) groups[group] = [];
+                groups[group].push([professor.name, professor.id]);
             });
         });
-        const students = users.filter(user => user.role == studentRole);
+        const students = users.filter(user => user.role == info.studentRole);
         const SGList = students.map(student => student.groups
-            .map(group => [courseId, student.name, student.id, student.mail, group])
+            .map(group => [info.courseId, student.name, student.id, student.mail, group])
         ).flat();
         const SPList = students.map(student => student.groups
-            .filter(group => group in classGroups).map(group => classGroups[group]
-                .map(professor => [courseId, student.name, student.id, professor]
+            .filter(group => group in groups).map(group => groups[group]
+                .map(professor => [info.courseId, student.name, student.id, professor]
                     .flat())).flat()).flat().filter((obj = {}, item => !(obj[item] = item in obj)));
 
         // Also update the UdGMoodle buttons
@@ -108,15 +102,15 @@
         }
         $("#udgmoodle_buttons .showall, #udgmoodle_buttons .sglist, #udgmoodle_buttons .splist").remove();
         if ($("nav.pagination").length) { // not the full list? link to it
-            $("#udgmoodle_buttons").append(`<a class="showall" href="${fullListUrl}">${locale.ALL_TEXT}</a>`);
+            $("#udgmoodle_buttons").append(`<a class="showall" href="${info.fullListUrl}">${info.lang.ALL_TEXT}</a>`);
         } else { // already in the full list
             if (SGList.length) {
-                $("#udgmoodle_buttons").append(`<a class="sglist" download="${courseId}_${locale.SGL_FILE}.csv"
-                    href="${tableToCsvDataUrl(SGList)}">${locale.SGL_TEXT}</a>`);
+                $("#udgmoodle_buttons").append(`<a class="sglist" download="${info.courseId}_${info.lang.SGL_FILE}.csv"
+                    href="${tableToCsvDataUrl(SGList)}">${info.lang.SGL_TEXT}</a>`);
             }
             if (SPList.length) {
-                $("#udgmoodle_buttons").append(`<a class="splist" download="${courseId}_${locale.SPL_FILE}.csv"
-                    href="${tableToCsvDataUrl(SPList)}">${locale.SPL_TEXT}</a>`);
+                $("#udgmoodle_buttons").append(`<a class="splist" download="${info.courseId}_${info.lang.SPL_FILE}.csv"
+                    href="${tableToCsvDataUrl(SPList)}">${info.lang.SPL_TEXT}</a>`);
             }
         }
 
@@ -125,9 +119,9 @@
     // CSS for the UdGMoodle buttons
     GM_addStyle('' + `
         #udgmoodle_buttons { text-align: right; width: 100% }
-        #udgmoodle_buttons a { background-color: steelblue; border-radius: 1.2em; color: white; display: inline-block;
+        #udgmoodle_buttons a { background-color: #568; border-radius: 1.2em; color: white; display: inline-block;
             margin: 4px 8px 0 0; padding: 0.425rem 1.25rem; text-decoration: none !important }
-        #udgmoodle_buttons a:hover { color: white; text-shadow: 1px 1px 2px black }
+        #udgmoodle_buttons a:hover { background-color: #78A; color: white; text-shadow: 0 1px 2px #000A }
     `);
 
     /*
@@ -138,7 +132,7 @@
     $(document).ready(function () {
 
         // Makes clear what script is running and its version
-        notification(GM.info.script.name + " " + GM.info.script.version, "hello", 3);
+        notification(GM.info.script[`name:${navigator.language.slice(0,2)}`] + " " + GM.info.script.version, "hello", 3);
 
         // Watches for changes in the page's main table
         const targetNode = document.getElementById("participantsform"); // node to watch
@@ -152,11 +146,16 @@
         const observer = new MutationObserver(callback); // associate the callback to an observer
         observer.observe(targetNode, config); // start monitoring the target for changes
 
-        // Some static details from the DOM
-        courseId = document.title.split("/")[0];
-        roles = getSelectOptions("select[data-field-name='roles']");
-        groups = getSelectOptions("select[data-field-name='groups']");
-        fullListUrl = String(document.location).replace(/(&perpage=\d+|perpage=\d+&)/img, "") + "&perpage=5000";
+        // Static details from the initial DOM
+        info.column = { "fullname": $("#participants tbody tr th").index() };
+        ["idnumber", "email", "roles", "groups"].forEach(i =>
+            info.column[i] = $(`#participants thead [data-column="${i}"]`).parents("th").index()
+        );
+        info.courseId = document.title.split("/")[0];
+        info.fullListUrl = String(document.location).replace(/(&perpage=\d+|perpage=\d+&)/img, "") + "&perpage=5000";
+        info.lang = langs[$("html").attr("lang").slice(0,2)] || langs.en;
+        info.noRole = $("select[data-field-name='roles'] option[value='-1']").text();
+        info.studentRole = $("select[data-field-name='roles'] option[value='5']").text();
 
         // Initial list generation
         let timer = setTimeout(updateLists);
@@ -174,7 +173,7 @@
     function notification(message, type = "info", timeout = 5) {
         // The "type" parameter accepts four values: "info", "warning", "error" and "hello"
         // The "timeout" parameter is expressed in seconds although Toastify uses milliseconds
-        var color, icon;
+        let color, icon;
         switch (type) {
             case "warning": color = "rgba(201, 201, 0, 0.8)"; icon = "⚠️"; break;
             case "error": color = "rgba(201, 51, 51, 0.8)"; icon = "🛑"; break;
