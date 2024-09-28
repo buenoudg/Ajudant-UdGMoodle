e1e550b8c2829b22e2a5dd7c33ba3a9850d968bd
Antonio Bueno <antonio.bueno@udg.edu>
2024-09-28 21:46:22 +0200

    Compatibilitat amb el nou Moodle i millors fotos
    Compatible amb gairebé tots els canvis introduïts a Moodle de l'any passat. S'ajusta la brillantor i el contrast només a les fotos que ho requereixen.
    
    Desactivada la generació del llistat d'estudiants per grup perquè s'ha de refer per al nou Moodle.

--- millores-participants.user.js
+++ millores-participants.user.js
@@ -3,15 +3,16 @@
 // @name:ca         Millores a "Participants"
 // @name:en         Improvements to "Participants"
 // @name:es         Mejoras en "Participantes"
-// @version         0.5.0
+// @version         0.8.0
 // @author          Antonio Bueno <antonio.bueno@udg.edu>
-// @description     Coloreja segons els rols, fa les fotos més grans, deixa fer el llistat imprimible i genera un llistat alumne/professor
-// @description:ca  Coloreja segons els rols, fa les fotos més grans, deixa fer el llistat imprimible i genera un llistat alumne/professor
-// @description:en  Highlights according to roles, makes photos bigger and better, allows making the list printable, and generates a user/teacher list
-// @description:es  Colorea según los roles, hace las fotos más grandes, deja hacer el listado imprimible y genera un listado alumno/profesora
+// @description     Coloreja segons els rols, fa les fotos més grans i millors, i deixa fer el llistat imprimible
+// @description:ca  Coloreja segons els rols, fa les fotos més grans i millors, i deixa fer el llistat imprimible
+// @description:en  Highlights according to roles, and makes photos bigger and better, and allows making the list printable
+// @description:es  Colorea según los roles, hace las fotos más grandes, y deja hacer el listado imprimible
 // @license         MIT
 // @namespace       https://github.com/buenoudg/Ajudant-UdGMoodle
 // @supportURL      https://github.com/buenoudg/Ajudant-UdGMoodle/issues
+// @match           https://moodle.udg.edu/user/index.php?*
 // @match           https://moodle2.udg.edu/user/index.php?*
 // @icon            https://raw.githubusercontent.com/buenoudg/Ajudant-UdGMoodle/master/udgmoodle_44x44.png
 // @require         https://cdn.jsdelivr.net/npm/jquery@3/dist/jquery.min.js
@@ -21,58 +22,78 @@
 // @grant           GM_getResourceText
 // ==/UserScript==
 
-// TODO: Rerun everything when page mutates
-// TODO: Apply extra brightness only to dark photos
-// TODO: Make the script independent of locale
-// TODO: Always offer the student×grup listing
+// TODO: Torna a executar tot quan la pàgina canviï
+// TODO: Fes que l'script sigui independent de la configuració regional
+// TODO: Restaura la llista d'estudiant×grup i fes que aparegui sempre
+
 
 // jshint esversion: 8
 
-// Fotos més grans i clares, dins un quadrat arrodonit, i amb més qualitat
+// Fotos amb més qualitat i dins d'un quadrat arrodonit
 function milloraFotos() {
-    const midaFoto = 70; // píxels
-    const brillantorExtra = 0.3; // TODO: Aclarar només les fotos fosques
-    GM_addStyle("" + `
+    GM_addStyle(`
         #participants img.userpicture {
             border-radius: 10%;
-            height: ${midaFoto}px !important;
-            width: ${midaFoto}px !important;
-            filter: brightness(${1 + brillantorExtra});
+            height: 70px !important;
+            width: 70px !important;
         }
-        #participants img.userpicture[src$=f3] {
-            filter: brightness(1);
-        }`);
+        #participants img.userpicture:hover {
+            border-radius: 10%;
+            height: 70px !important;
+            width: 70px !important;
+            filter: brightness(1) contrast(1) !important;
+        }
+    `);
+
     $("#participants img.userpicture").each(function () {
-        $(this).attr("src", $(this).attr("src").replace(/\/f[12]/, "/f3")); // fa servir fotos al màxim de qualitat
+        // Canvas temporal per analitzar la imatge
+        const canvas = document.createElement('canvas');
+        const ctx = canvas.getContext('2d');
+        canvas.width = this.width;
+        canvas.height = this.height;
+        ctx.drawImage(this, 0, 0, this.width, this.height);
+        const imgData = ctx.getImageData(0, 0, this.width, this.height);
+        const data = imgData.data;
+
+        // Recorrem tots els píxels per calcular la brillantor i el contrast
+        let sumaBrillantor = 0;
+        let sumaQuadratsBrillantor = 0;
+        for (let i = 0; i < data.length; i += 4) {
+            const brillantor = 0.2126 * data[i] + 0.7152 * data[i+1] + 0.0722 * data[i+2];
+            sumaBrillantor += brillantor;
+            sumaQuadratsBrillantor += brillantor * brillantor;
+        }
+
+        // Calcula la brillantor i el contrast
+        const totalPixels = this.width * this.height;
+        const brillantor = sumaBrillantor / totalPixels;
+        const contrast = Math.sqrt(sumaQuadratsBrillantor / totalPixels - brillantor * brillantor);
+
+        // Aplicar els filtres CSS si cal
+        const filtre = `brightness(${brillantor<100 ? 1.3 : (brillantor>210 ? 0.9 : 1)}) contrast(${contrast<40 ? 1.5 : (contrast>150 ? 0.9 : 1)})`;
+        if (filtre != 'brightness(1) contrast(1)') {
+          console.log(filtre, $(this).parent().text(), `brillantor = ${brillantor}, contrast = ${contrast}`);
+          $(this).css('filter', filtre);
+        }
+
+        // Actualitza la foto a la màxima qualitat
+        $(this).attr("src", $(this).attr("src").replace(/\/f[12]/, "/f3"));
     });
 }
+
 
 // Colora els participants no estudiants
 function coloraRols(professor = "#EDC", coordinador = "#CDE", senseRol = "#F99") {
     GM_addStyle("" + `
-        #participants tr.professor td, #participants tr.professor-no-editor td { background-color: ${professor} }
-        #participants tr.coordinador td { background-color: ${coordinador} }
-        #participants tr.sense-rols td { background-color: ${senseRol} }
+        #participants tr.professor td, #participants tr.professor th,
+        #participants tr.professor-no-editor td, #participants tr.professor-no-editor th { background-color: ${professor} }
+        #participants tr.coordinador td, #participants tr.coordinador th,
+        #participants tr.sotsdirector td, #participants tr.sotsdirector th{ background-color: ${coordinador} }
+        #participants tr.sense-rols td, #participants tr.sense-rols th { background-color: ${senseRol} }
     `);
     $("#participants td.c4").each(function () { // afegeix classes CSS basades en els noms dels rols
         $(this).parents("tr").addClass($(this).text().trim().toLowerCase().replace(/ /g, "-"));
     });
-}
-
-// Extreu dades de la taula de participants
-function dadesParticipants() {
-    let participants = [];
-    $("#participants tbody tr").not(".emptyrow").each(function () {
-        const participant = {
-            "cognomsNom": $(this).find(".c1").text().trim(),
-            "numeroUdG": $(this).find(".c2").text().trim(),
-            "rol": $(this).find(".c4").text().trim(), // s'assumeix que només hi ha un rol per participant
-            "grups": $(this).find(".c5").text().trim()
-        };
-        participant.grups = (participant.grups == "Sense grups") ? [] : participant.grups.split(", ");
-        participants.push(participant);
-    });
-    return participants;
 }
 
 (function () {
@@ -90,68 +111,47 @@ function dadesParticipants() {
 
         milloraFotos();
         coloraRols();
-        let participants = dadesParticipants();
 
-        // Genera llistats només quan tots els participants es veuen a la pàgina
-        if (document.location.href.includes("perpage=5000") || $("a[data-target-page-size]").length == 0) {
-            // Dades derivades a partir de les dades extretes
-            const codiAssignatura = document.title.split("/")[0];
-            const professors = participants.filter(participant => (participant.rol != "Estudiant") && (participant.grups.length > 0));
-            let grups = {};
-            professors.forEach(function (professor) {
-                professor.grups.forEach(function (grup) {
-                    if (!grups[grup]) grups[grup] = [];
-                    grups[grup].push(professor.cognomsNom + ";" + professor.numeroUdG);
-                });
-            });
-            const estudiants = participants.filter(participant => participant.rol == "Estudiant");
-            // Relació alumne/professor (o alumne/grup si el professor del grup és desconegut)
-            let llistat = [];
-            estudiants.forEach(function (estudiant) {
-                estudiant.grups.forEach(function (grup) {
-                    if (grup in grups) {
-                        grups[grup].forEach(function (professor) {
-                            llistat.push([codiAssignatura, estudiant.cognomsNom, estudiant.numeroUdG, professor].join(";"));
-                        });
-                    } else {
-                        llistat.push([codiAssignatura, estudiant.cognomsNom, estudiant.numeroUdG, grup].join(";"));
-                    }
-                });
-            });
-            const CSV = [... new Set(llistat)].join("\r\n");
-            const dadesLlistat = (Object.keys(grups).length > 0) ? "alumne×professor" : "alumne×grup";
-            $(".userlist").prepend(`<a href="data:text/plain;charset=utf-8,\uFEFF${encodeURIComponent(CSV)}"
-                download="${codiAssignatura}.csv" style="float:right">💾 Descarrega llistat ${dadesLlistat}`);
-        } else {
-            // Clona l'enllaç "Mostra'ls tots" (si existeix) a sobre de la llista de participants
-            // i força la recàrrega de la pàgina en fer clic a qualsevol d'aquests dos enllaços
-            GM_addStyle("" + `
-                #show-all-clone { float: right }
-                #show-all { position: relative; top: -3em }
-            `);
-            $(".userlist").prepend($("#participantsform a[href*='perpage=5000']").clone().attr("id", "show-all-clone"));
-            $(".table-dynamic").after($("#participantsform a[href*='perpage=5000']").attr("id", "show-all"));
-        }
+        // Clona l'enllaç "Mostra'ls tots" (si existeix) a sobre de la llista de participants
+        // i força la recàrrega de la pàgina en fer clic a qualsevol d'aquests dos enllaços
+        GM_addStyle("" + `
+            #show-all-clone { float: right }
+            #show-all { position: relative; top: -3em }
+        `);
+        $(".userlist").prepend($("#participantsform a[href*='perpage=5000']").clone().attr("id", "show-all-clone"));
+        $(".table-dynamic").after($("#participantsform a[href*='perpage=5000']").attr("id", "show-all"));
+
+        // Elimina les inicials que aparèixen quan no tenen foto
+        $("#participants").find(".userinitials").remove();
 
         // Fes imprimible el llistat (filtrat o no)
         GM_addStyle("#printable { border-radius: 1.2em; margin-right: 1em; padding: 0.3em 0.9em; }");
         $(".enrolusersbutton form div").first().prepend('<button id="printable">Fes-ho imprimible');
-        $("#printable").on("click", function () {
+        $("#printable").on("click", function() {
             milloraFotos();
             $("body").prepend($("#participants"));
             GM_addStyle("" + `
-                nav, #page, #sidepreopen-control, #sidepre-blocks, #nav-drawer,
+                nav, #page, #sidepreopen-control, #sidepre-blocks, #nav-drawer, #page-wrapper,
                 #top-footer, #nav-drawer-footer, .c0, .c1 a, .c2, .c3, .c4 span, .c5 span, .c6, .c7, thead { display: none !important }
-                body { margin: 0 !important }
+                html body { background-color: white !important; margin: 0 !important }
                 table, tr, th, td { background-color: white !important; border: 1px solid black !important; color: black !important }
                 .c1 { white-space: nowrap; width: 34% }
                 .c1 img { float: left }
                 .c1 div { font-weight: normal !important }
             `);
+
+            // Elimina de nou les inicials que aparèixen quan no tenen foto
+            $("#participants").find(".userinitials").remove();
+
+            $("body").prepend('<h1 id="title" contenteditable="true">'+document.title);
+            $("#title").on("keyup", function() {
+              document.title = $(this).text().replace(":", " - ").replace("/", "-").replace(".", "");
+            });
+
             $("#participants tr").each(function() {
                 const name = $(this).find(".c1").text();
                 const id = $(this).find(".c2").text();
-                const email = $(this).find(".c3").text();
+                const email = $(this).find(".c5").text();
                 const $photo = $(this).find(".c1 img");
                 $(this).find(".c1").prepend($photo).append(`<div><b>${name}<div>${id}<div>${email}`);
             });
